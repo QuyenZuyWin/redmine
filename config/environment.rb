7e1d1b7ce9018c25ccbc9e847b1d22cbf91cde8b
ndthinh.sgt <ndthinh.sgt@gmail.com>
2023-03-13 09:25:16 +0700

    #init

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
-# Load the Rails application.
-require_relative "application"
+# frozen_string_literal: true
 
-# Initialize the Rails application.
+# Load the Rails application
+require File.expand_path('../application', __FILE__)
+
+# Make sure there's no plugin in vendor/plugin before starting
+vendor_plugins_dir = File.join(Rails.root, "vendor", "plugins")
+if Dir.glob(File.join(vendor_plugins_dir, "*")).any?
+  $stderr.puts "Plugins in vendor/plugins (#{vendor_plugins_dir}) are no longer allowed. " \
+    "Please, put your Redmine plugins in the `plugins` directory at the root of your " \
+    "Redmine directory (#{File.join(Rails.root, "plugins")})"
+  exit 1
+end
+
+# Initialize the Rails application
 Rails.application.initialize!
