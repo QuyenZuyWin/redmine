ee04920cb2dc268706dfdbd66de06fbc80574aba
ndthinh.sgt <ndthinh.sgt@gmail.com>
2023-03-13 13:04:44 +0700

    #add

---
+++ config/initializers/puma.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env puma
+require 'puma'
+
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads threads_count, threads_count
+
+preload_app!
+
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
